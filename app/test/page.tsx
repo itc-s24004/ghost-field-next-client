76e58e9147067fe5e59040e347cd4310b99ec8d0
s24004 <s24004@std.it-college.ac.jp>
2026-01-29 13:57:47 +0900

    テスト用ページを追加

---
+++ app/test/page.tsx
@@ -0,0 +1,11 @@
+import { UI_Card } from "@/page_components/game/card";
+import { PlainTitle } from "@/page_components/title";
+
+export default function Page() {
+    return (
+        <div>
+            <PlainTitle>Test Page</PlainTitle>
+            <UI_Card data={{}} />
+        </div>
+    )
+}

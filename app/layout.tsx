c4c2e74fc29b953447cb684666da0038ec77c3fd
s24004 <s24004@std.it-college.ac.jp>
2026-02-11 21:58:31 +0900

    全体で使用するフォントを変更

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
+
+import { Zen_Kurenaido } from "next/font/google";
+
+const zenKurenaido = Zen_Kurenaido({
+  weight: ["400"],
+});
+
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Ghost Field Next Client",
@@ -25,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="ja">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${zenKurenaido.className} antialiased`}
       >
         {children}
       </body>

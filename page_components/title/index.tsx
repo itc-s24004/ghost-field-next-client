c229a2bf25f4e65cfa78424ec67786d0ab982f01
s24004 <s24004@std.it-college.ac.jp>
2026-02-11 22:12:27 +0900

    全体で使用するタイトル

--- page_components/title/index.tsx
+++ page_components/title/index.tsx
@@ -1,10 +1,23 @@
 import { MergeAttributes } from "@/libs/customAttribute";
 import styles from "./index.module.css";
+import { SizeType } from "@/types";
 
 
+type Props = React.HTMLAttributes<HTMLElement> & {
+    size?: SizeType;
+};
 
-export function PlainTitle(props: React.HTMLAttributes<HTMLElement>) {
+const classNameMap: Record<SizeType, string> = {
+    small: styles.small,
+    medium: styles.medium,
+    large: styles.large,
+    extraLarge: styles.extraLarge,
+    mega: styles.mega
+};
+
+
+export function PlainTitle({ size = "medium", ...props }: Props) {
     return (
-        <h1 {...MergeAttributes(props, { className: styles.title })} />
+        <h1 {...MergeAttributes(props, { className: `${styles.title} ${classNameMap[size]}` })} />
     )
 }

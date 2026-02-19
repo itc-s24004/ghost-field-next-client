0abdfd0e794196c0297ff4024eb52345660a4ea9
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 18:18:39 +0900

    ルーム作成画面を追加

--- page_components/app/_screen/roomCreate/screen.tsx
+++ page_components/app/_screen/roomCreate/screen.tsx
@@ -1,17 +1,27 @@
+import { UI_Form } from "@/page_components/form";
 import { AppScreen, Screen_Frame } from "@/screen/screen_frame";
+import { useState } from "react";
 
 type Props = AppScreen & {
-    _onCreate(): void;
+    API_Server_URL: URL;
+    onCreate(): void;
 };
 
-export function Screen_RoomCreate({ media, _onCreate, ...props }: Props) {
+export function Screen_RoomCreate({ media, API_Server_URL, onCreate, ...props }: Props) {
+    const [roomName, setRoomName] = useState("");
+    
+    
     return (
         <Screen_Frame
             {...props}
         >
 
             <div>
-                Room Create Screen
+                <UI_Form>
+                    <label>ルーム名(ID)</label>
+                    <input type="text" max={36} value={roomName} onChange={(e) => setRoomName(e.target.value)}/>
+                    <button type="submit">作成</button>
+                </UI_Form>
             </div>
 
         </Screen_Frame>

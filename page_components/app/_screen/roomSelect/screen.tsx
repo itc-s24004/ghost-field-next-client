a87e5af76445d8991105fbbe6f0ecd703b4f5d90
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 18:20:12 +0900

    ルーム選択画面を追加

--- page_components/app/_screen/roomSelect/screen.tsx
+++ page_components/app/_screen/roomSelect/screen.tsx
@@ -1,15 +1,34 @@
 import { AppScreen, Screen_Frame } from "@/screen/screen_frame";
 import { Room } from "@/page_components/app/room";
 import { Plain_Button } from "@/page_components/form/input/button";
+import { useEffect, useState } from "react";
+import { API } from "@/libs/api/api";
+import { Popup_Direct_Connect } from "../../_popup/direct_connect/popup";
+
+import styles from "./screen.module.css";
+
 
 type Props = AppScreen & {
-    data: unknown;
-    selectRoom: (roomId: string) => void;
-    onJoin(serverUrl: string): void;
+    API_Server_URL: URL;
+    connect: (server: URL) => void;
 }
 
 
-export function Screen_RoomSelect({ media, data, selectRoom, onJoin, children, ...props }: Props) {
+export function Screen_RoomSelect({ media, API_Server_URL, connect, children, ...props }: Props) {
+    const [rooms, setRooms] = useState<API.RoomData[]>([]);
+
+    const { element: popupElement, controller: popupController } = Popup_Direct_Connect({
+        onConnect: (serverURL) => connect(serverURL)
+    });
+    
+    
+    useEffect(() => {
+        API.API_RoomList().then(res => {
+            if (res.success) {
+                setRooms(res.data ?? []);
+            }
+        });
+    }, []);
 
 
     return (
@@ -18,15 +37,19 @@ export function Screen_RoomSelect({ media, data, selectRoom, onJoin, children, .
 
             _top={
                 <div style={{display: "flex", flexDirection: 'row-reverse'}}>
-                    <Plain_Button size="small" style={{ }}>ダイレクト接続</Plain_Button>
+                    <Plain_Button size="small" style={{ }} onClick={() => popupController.show()}>サーバーに接続</Plain_Button>
                 </div>
             }
 
         >
+            {popupElement}
 
-
-            <div>
-                <Room data={data} onClick={() => selectRoom("roomid")}/>
+            <div className={styles.container}>
+                {
+                    rooms.map(room => (
+                        <Room key={room.id} data={room} onClick={() => connect(new URL(`/${room.id}`, API_Server_URL))}/>
+                    ))
+                }
             </div>
 
             

c3a189e8dc34b42e4bf06129c4d2a108d52ed1d0
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 18:18:06 +0900

    ホーム画面を追加

--- page_components/app/_screen/home/screen.tsx
+++ page_components/app/_screen/home/screen.tsx
@@ -5,30 +5,69 @@ import { PlainTitle } from "@/page_components/title"
 import { SizeType } from "@/types";
 
 import styles from "./screen.module.css";
+import { Popup_CreateRoom } from "../../_popup/create_room/popup";
+import { ScreenMap_2 } from "@/screen/screen";
+import { Screen_Game } from "../game/screen";
+import { useState } from "react";
+import { API_SERVER_URL } from "@/libs/api/base";
 
 
+
+
+
+type ScreenIDs = "home" | "game";
+
+type SelectType = "create" | "join";
 
 type Props = AppScreen & {
-    onCreateRoom: () => void;
-    onJoinRoom: () => void;
+    imageURLs: string[];
+    select(type: SelectType): void;
 };
 
 
 
-export function Screen_Home({ media, onCreateRoom, onJoinRoom, ...props }: Props) {
+export function Screen_Home({ media, imageURLs, select, ...props }: Props) {
     const size: SizeType = media === "mobile" ? "large" : "mega";
-    return (
+
+
+    const {element: createRoomElement, controller: createRoomController} = Popup_CreateRoom({
+        media,
+        imageURLs,
+        onCreate: (roomName) =>  {
+            setServerURL(new URL(`/${roomName}`, API_SERVER_URL));
+            setScreen("game");
+        }
+    })
+    
+    
+    const [serverURL, setServerURL] = useState<URL | null>(null);
+    
+    const [screen, setScreen] = useState<ScreenIDs>("home");
+
+    const screens: ScreenMap_2<ScreenIDs> = {
+        "home":
         <Screen_Frame
         >
-
-
+            {createRoomElement}
             <div {...MergeAttributes(props, { className: styles.container })}>
                 <PlainTitle size={size}>Ghost Field</PlainTitle>
-                <Plain_Button size={size} onClick={onCreateRoom}>ルームを作成</Plain_Button>
-                <Plain_Button size={size} onClick={onJoinRoom}>ルームに参加</Plain_Button>
+                <Plain_Button size={size} onClick={createRoomController.show}>ルームを作成</Plain_Button>
+                <Plain_Button size={size} onClick={() => select("join")}>ルームに参加</Plain_Button>
             </div>
 
 
-        </Screen_Frame>
+        </Screen_Frame>,
+        "game":
+        <Screen_Game
+            media={media}
+            serverURL={serverURL!}
+            _onClickBackward={() => setScreen("home")}
+        />
+    }
+    
+    
+    
+    return (
+        screens[screen]
     )
 }

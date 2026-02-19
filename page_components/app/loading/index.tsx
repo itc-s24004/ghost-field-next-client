952acf8fcd37c9a31018096bbc4d7b311df1c6d3
s24004 <s24004@std.it-college.ac.jp>
2026-02-19 18:16:53 +0900

    ロード画面を追加

--- page_components/app/loading/index.tsx
+++ page_components/app/loading/index.tsx
@@ -3,9 +3,9 @@ import styles from "./index.module.css";
 import { UI_Screen_Fill } from "@/page_components/screen";
 
 
-export function APP_Loading(props: React.HTMLAttributes<HTMLElement>) {
+export function APP_Loading({ children, ...props }: React.HTMLAttributes<HTMLElement>) {
     return (
-        <UI_Screen_Fill {...MergeAttributes(props, { className: styles.container })}>
+        <UI_Screen_Fill _type="opaque" {...MergeAttributes(props, { className: styles.container })}>
             <div className={styles.bone} />
             <div className={styles.bone} />
             <div className={styles.bone} />
@@ -19,7 +19,11 @@ export function APP_Loading(props: React.HTMLAttributes<HTMLElement>) {
 
 
                 <div className={styles.spinner}></div>
-                <div className={styles.text}>Loading...</div>
+                {
+                    children && typeof children !== "string" ? 
+                    children :
+                    <div className={styles.text}>{children ?? "Loading..."}</div>
+                }
             </div>
         </UI_Screen_Fill>
     );
